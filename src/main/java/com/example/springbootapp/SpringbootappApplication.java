6e1cf19be0475764a07abaab1b081999bbc9be37
paweljumper20 <paweljumper20@gmail.com>
2020-02-20 20:27:59 +0100

    Spring start 20.02

---
+++ src/main/java/com/example/springbootapp/SpringbootappApplication.java
@@ -0,0 +1,15 @@
+package com.example.springbootapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan
+public class SpringbootappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootappApplication.class, args);
+	}
+
+}
